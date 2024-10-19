a85abc115816a3b14eb9f9ba3d42bb84b46b18f9
4nktt <179658021+4nktt@users.noreply.github.com>
2024-10-19 11:39:15 +0530

    chore: remove extra content and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,17 @@
-import { useState } from "react";
-
-import reactLogo from "./assets/react.svg";
-// eslint-disable-next-line import/no-unresolved
-import viteLogo from "/vite.svg";
-
-import "./app.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
+    <main>
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img
+          width={50}
+          height={50}
+          src="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📦</text></svg>"
+          alt="Package logo"
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => { setCount((count) => count + 1); }}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <h1>Vite Template</h1>
+      <p>Starter template for Vite and React</p>
+    </main>
   );
 }
 
